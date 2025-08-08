62db5bc7e9795d6017dc3a1686b4cdad69b380fd
Korakod <ice9393th@gmail.com>
2025-08-08 13:27:29 +0700

    Add dialog and Seachbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Tabel from './assets/Component/Tabel'
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+
+ 
   return (
     <>
     <main>
-      <div className=' bg-gray-400 p-2'>
-        <input onChange={console.log("Test")} className='bg-gray-200 p-2 rounded-2xl' placeholder='ค้นหา' type='text'></input>
-      </div>
- <Tabel/>
+
+ <Tabel />
     </main>
     </>
   )
--- src/assets/Component/Tabel.jsx
+++ src/assets/Component/Tabel.jsx
@@ -1,24 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useRef } from 'react'
 import itemStore from "/Product/ItemStore.json"
 import { useState } from 'react';
 export default function Tabel() {
   const [Product, setProduct] = useState([])
   const [loading,setloading] = useState()
+  const [update,setupdate] = useState("")
  
-  useEffect(()=>{
-    try {
-       fetch('https://fakestoreapi.com/products')
-  .then(response => response.json())
-  .then(data => setProduct(data));
-    } catch (error) {
-      console.error(error)
-    }
-  },[])
-console.log(Product)
-  
 
+  const updateitem = itemStore.filter((item)=>
+  item.Name.toLowerCase().includes(update.toLowerCase())
+)
+ const DialogRef = useRef() 
+ const OpenDialog = (item) =>{
+  DialogRef.current.showModal()
+  setProduct(item)
+ }
+ const CloseDialog = () =>{
+  DialogRef.current.close()
+ }
   return (
   <div>
+     <div className=' bg-gray-400 p-2'>
+        <input value={update} onChange={(e)=>setupdate(e.target.value)} className='bg-gray-200 p-2 rounded-2xl' placeholder='ค้นหา' type='text'></input>
+      </div>
          <table className="md:min-w-full  bg-gray-800 text-white border border-gray-600 w-full">
         <thead>
           <tr className="border-b border-gray-600 md:text-lg  text-[12px]">
@@ -30,17 +34,29 @@ console.log(Product)
           </tr>
         </thead>
         <tbody >
-         {Product.map((item,index)=>{
+         {updateitem.map((item,index)=>{
           return <tr key={index} className='border-b border-gray-600'>
           <td >{item.id}</td>
-          <td>{item.title}</td>
+          <td>{item.Name}</td>
           <td>{item.total}</td>
-         <td> <button type='button' className=' bg-gray-200  text-black mt-2 mb-2 px-2 rounded-sm text-sm'>รายละเอียด</button></td>
+         <td> <button type='button' className=' bg-gray-200  text-black mt-2 mb-2 px-2 rounded-sm text-sm'
+         onClick={()=>OpenDialog(item)}
+         >รายละเอียด</button></td>
          </tr>
          })}
          
         </tbody>
       </table>
+      <dialog ref={DialogRef} className=' fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+             w-[400px] p-4 bg-white rounded-md shadow-lg border-4 border-blue-300'>
+        <div className=''>
+        <div >
+          <h1>รหัส: {Product.id}</h1>
+        <h1>ชื่อสินค้า: {Product.Name}</h1>
+        <h1>จำนวน: {Product.total}</h1></div>
+        <div><button className='bg-gray-400 px-2 rounded-md mt-5 hover:bg-gray-500' onClick={CloseDialog}>ปิด</button></div>
+        </div>
+      </dialog>
       </div>
   )
 }
